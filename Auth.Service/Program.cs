9fa2b0f772c75c132a27c4bcbe7f9d7e6fed8d53
Camilo Camacho <hiphap134@gmail.com>
2026-03-11 20:37:16 -0500

    Configuración JWT en Program.cs

--- Auth.Service/Program.cs
+++ Auth.Service/Program.cs
@@ -2,6 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(options => {
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters {
+        ValidateIssuer = true,
+        ValidIssuer = "NetflixCloneAuth",
+        ValidateAudience = true,
+        ValidAudience = "NetflixCloneUsers",
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Esta_Es_Una_Llave_Super_Secreta_De_Al_Menos_32_Caracteres_123!"))
+    };
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
