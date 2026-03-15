e414788e181bee526e34e4ac21afdb4836b6fd2d
Karen Ocampo <okaren.ojimena@gmail.com>
2026-03-15 15:06:29 -0500

    fix(streaming): enable CORS and finalize video serving engine

--- Streaming.Service/Program.cs
+++ Streaming.Service/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 // --- CONFIGURACIÓN DE STREAMING ---
 app.UseStaticFiles(new StaticFileOptions
